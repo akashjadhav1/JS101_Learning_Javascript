5f63a0e3fc9dd32b4896dd602a4b0b8f8d48ec61
akashjadhav1 <123940829+akashjadhav1@users.noreply.github.com>
2023-01-30 17:06:29 +0530

    Update problem1.js

---
+++ problem1.js
@@ -0,0 +1,5 @@
+//Print "Masai School" in the console followed by "A Transformation in Education" in next line
+let akash ="Masai School";
+console.log(akash);
+let jadhav ="A Transformation in Education";
+console.log(jadhav);
